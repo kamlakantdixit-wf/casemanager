1d2be2fb663f8f72031e6d9f7829fb1361f62746
kamlakantdixit-wf <kamla.kant.dixit@gmail.com>
2024-09-06 07:24:21 -0400

    corrected javax jakarta imports

--- src/main/java/com/citi/casemanager/CaseManagementApplication.java
+++ src/main/java/com/citi/casemanager/CaseManagementApplication.java
@@ -2,8 +2,13 @@ package com.citi.casemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.citi.casemanager.model"})
+@EnableJpaRepositories(basePackages = {"com.citi.casemanager.repository"})
+
 public class CaseManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(CaseManagementApplication.class, args);
--- src/main/java/com/citi/casemanager/repository/CaseRepository.java
+++ src/main/java/com/citi/casemanager/repository/CaseRepository.java
@@ -2,6 +2,8 @@ package com.citi.casemanager.repository;
 
 import com.citi.casemanager.model.Case;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CaseRepository extends JpaRepository<Case, Long> {
 }
